10771f86a66d67e6e8ca5357d892220946ead01d
Ross Perry <perryr16@gmail.com>
2021-01-22 12:58:03 -0700

    daya displaying correctly

--- dj_pg_tut/cars/views.py
+++ dj_pg_tut/cars/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from cars.models import Car, Driver
 # Create your views here.
 def car_detail(request, pk):
-  owner_obj = Driver.objets.get(pk=pk)
+  owner_obj = Driver.objects.get(pk=pk)
   car_objs = Car.objects.filter(owner_id=owner_obj.id)
   context = {
     "vehicles": car_objs,
-    "drivers": owner_objs,
+    "drivers": owner_obj,
   }
   return render(request, "car_detail.html", context)
